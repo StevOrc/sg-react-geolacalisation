50b4ded269c9d2d851ca89fc1b9faf0e414824d8
StevOrc <steven.orcun2@gmail.com>
2020-11-05 20:02:55 +0100

    adding css

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,30 +1,33 @@
+import './SeasonDisplay.css';
 import React from "react";
 
 const seasonConfig = {
   summer: {
-    text: 'Il y a du soleil',
-    iconName: 'sun'
+    text: "Il y a du soleil",
+    iconName: "sun",
   },
   winter: {
-    text: 'Il fait froid',
-    iconName: 'snowflake'
-  }
-}
+    text: "Il fait froid",
+    iconName: "snowflake",
+  },
+};
 
 const getSeason = (lat, month) => {
-  if(month > 2  && month < 9) return lat > 0 ? 'summer' : 'winter';
-  else return lat > 0 ? 'winter' : 'summer';
-}
+  if (month > 2 && month < 9) return lat > 0 ? "summer" : "winter";
+  else return lat > 0 ? "winter" : "summer";
+};
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const {text, iconName} = seasonConfig[season];
-  console.log("AAA", iconName)
-  return <div>
-    <i className={`massive ${iconName} icon`}></i>
-    <h1>{text}</h1>
-    <i className={`massive ${iconName} icon`}></i>
-    </div>;
+  const { text, iconName } = seasonConfig[season];
+  
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`}></i>
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`}></i>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import SeasonDisplay from './SeasonDisplay'
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 export default class App extends Component {
+  state = { lat: null, errorMessage: "" };
 
-  state = {lat : null, errorMessage: ''};
-
-  componentDidMount(){
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) =>  this.setState({lat: position.coords.latitude}),
-      (error) => this.setState({errorMessage: error.message})
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (error) => this.setState({ errorMessage: error.message })
     );
   }
-
-  componentDidUpdate() {}
 
   render() {
     if (this.state.errorMessage && !this.state.lat) {
@@ -21,10 +19,10 @@ export default class App extends Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay  lat={this.state.lat} />
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading </div>;
+    return <Spinner />;
   }
 }
 
